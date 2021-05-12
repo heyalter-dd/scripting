101fe4e7d6e684189c06e352b6e9c1d42d2f1c8e
heyalter-dd <84018432+heyalter-dd@users.noreply.github.com>
2021-05-12 09:06:59 +0200

    Add files via upload

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,19 @@
-/var/www/html/ubuntu/setup/cleanup.sh
+#!/bin/bash
+# modified TM 2021, umount smb
+if [ ! -f /opt/setup/setup_done ]
+then
+zenity --warning --text "Erst setup.sh ausführen!" --width 512
+exit 2
+fi
+
+find /home -lname '/opt/setup/*' -delete
+cp /opt/setup/heyalterhelp.desktop ~/Schreibtisch
+gio set ~/Schreibtisch/heyalterhelp.desktop "metadata::trusted" true
+
+# umount smb share
+# ip anpassen!
+gio mount -u smb://192.168.10.20/transfer
+
+/opt/setup/cleanuproot.sh
+
+notify-send -t 1 "Finalisierung abgeschlossen!"
