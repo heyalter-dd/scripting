5bb92768410301e2e71fe8247d84a8c8f8efb7c6
heyalter-dd <84018432+heyalter-dd@users.noreply.github.com>
2021-05-12 09:06:20 +0200

    Add files via upload

--- ubuntu-post.sh
+++ ubuntu-post.sh
@@ -1,1 +1,46 @@
-/var/www/html/transfer/ubuntu-post.sh
+#!/bin/sh
+#install hardinfo and create benchmark report to CIFS share
+#(c) TM 2021
+#
+zenity --timeout 1 --info --text="Installation wird finalisiert..."
+clear
+vendor=`sudo dmidecode -s system-manufacturer | sed 's/\ //g'`
+model=`sudo dmidecode -s system-product-name | sed 's/\ //g'`
+serial=`sudo dmidecode -s system-serial-number | sed 's/\ //g'`
+datum=`date +%d-%m-%Y`
+folder=`echo $vendor"_"$model"_"$serial"-"$datum`
+#sudo chmod -R a+rw reports/
+if test ! -e reports/$folder; then 
+ echo "directory not existing, creating it"
+ mkdir -p reports/$folder
+fi
+sudo apt update -y
+sudo apt install hardinfo -y
+zenity --timeout 1 --info --text="Benchmark wird erstellt..."
+hardinfo -r -f html > /tmp/sysreport.html
+cat /tmp/sysreport.html | sed -n '/\<html/,/\<\/html/p' > reports/$folder/sysreport.html
+rm /tmp/sysreport.html
+zenity --timeout 1 --info --text="Systemreport wurde erstellt"
+#zenity --info --text "Systemreport auf dem Staging Server unter "$folder" abgelegt" --title="Systemreport"
+# updates 
+zenity --timeout 1 --info --text="Updates werden installiert, bitte warten..."
+# packages
+sudo apt upgrade -y
+# snap
+zenity --timeout 1 --info --text="Snaps werden aktualisiert, bitte warten..."
+#snap list | awk '{print $1}' | sed 's/Name//g' > snapupdatelist
+#sudo while read line; do snap refresh $line; done < snapupdatelist
+#rm snapupdatelist
+snap refresh
+#
+# TM 11-05-21 SN dazu
+if zenity --question --ellipsize --text="Finalisierung beendet. S/N :"$serial" notieren! Cleanup-Script jetzt starten?"; then
+ zenity --timeout 2 --info --text="Cleanup ..."
+ /home/schule/Schreibtisch/./cleanup.sh
+ pkill update-manager
+ /snap/bin/chromium https://www.heyalter.com/dresden &
+else
+ zenity --info --text="Cleanup nicht vergessen!"
+ pkill gnome-terminal
+fi
+pkill gnome-terminal
