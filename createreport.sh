ae17734c3e582b6b9da2ae8a5d057624d86ec7c2
heyalter-dd <thomi1669@gmail.com>
2021-05-14 10:06:36 +0200

    sudo zur reporterstellung

--- createreport.sh
+++ createreport.sh
@@ -8,13 +8,13 @@ model=`dmidecode -s system-product-name | sed 's/\ //g'`
 serial=`dmidecode -s system-serial-number | sed 's/\ //g'`
 datum=`date +%d-%m-%Y`
 folder=`echo $vendor"_"$model"_"$serial"-"$datum`
-mkdir -p reports/$folder
+sudo mkdir -p reports/$folder
 if [ ! -d /home/partedmagic/diskveri* ]; then
 zenity --error --text "Loeschvorgang wirklich erledigt?" --title="Keine Reports vorhanden"
 exit 0
 else
-cp /home/partedmagic/Secure-* reports/$folder
-cp /home/partedmagic/diskveri* reports/$folder
+sudo cp /home/partedmagic/Secure-* reports/$folder
+sudo cp /home/partedmagic/diskveri* reports/$folder
 fi
 #errorhandling here
 zenity --info --text "Report auf dem Staging Server unter "$folder" abgelegt" --title="Loeschreport"
