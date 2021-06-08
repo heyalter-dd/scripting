8f429c50cd21dea14808aade5a1f5c80b943b5b5
heyalter-dd <84018432+heyalter-dd@users.noreply.github.com>
2021-06-08 19:39:32 +0200

    Update createreport.sh

--- transfer/createreport.sh
+++ transfer/createreport.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#create reports from pmagic to CIFS share
+#create reports from pmagic to CIFS/NFS share
 #(c) TM 2021
 #
 clear
@@ -8,14 +8,12 @@ model=`dmidecode -s system-product-name | sed 's/\ //g'`
 serial=`dmidecode -s system-serial-number | sed 's/\ //g'`
 datum=`date +%d-%m-%Y`
 folder=`echo $vendor"_"$model"_"$serial"-"$datum`
-<<<<<<< HEAD:transfer/createreport.sh
 mkdir -p reports/$folder
 chmod -R a+rw reports/$folder
 if [ ! /home/partedmagic/diskveri* ]; then
 =======
 sudo mkdir -p reports/$folder
 if [ ! -d /home/partedmagic/diskveri* ]; then
->>>>>>> aa568340838363f24dc6b66cbb5a7c1391ca64c3:createreport.sh
 zenity --error --text "Loeschvorgang wirklich erledigt?" --title="Keine Reports vorhanden"
 exit 0
 else
